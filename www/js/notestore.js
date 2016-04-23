42efbead3fc2a68232aa5eea13a3952b0c911de5
italoaguiar <italofaguiar@gmail.com>
2016-04-23 17:26:03 -0300

    reabilitando para o backend com mongodb

--- www/js/notestore.js
+++ www/js/notestore.js
@@ -6,7 +6,8 @@ angular.module('mynotes.notestore', [])
     //var apiUrl = 'https://notas-italo-backend.herokuapp.com';
     //var apiUrl = 'http://ws.tocae.com.br';
     //var apiUrl = 'http://192.168.0.195:8200';
-    var apiUrl = 'http://localhost:8200';
+    //var apiUrl = 'http://localhost:8200';
+    var apiUrl = 'http://localhost:8300';
 
     return {
 
@@ -18,7 +19,7 @@ angular.module('mynotes.notestore', [])
 
       get: function (noteId) {
         return $http.get(apiUrl + '/notes/' + noteId).then(function (response) {
-          return response.data;
+          return response.data[0];
         });
       },
 
@@ -27,7 +28,7 @@ angular.module('mynotes.notestore', [])
       },
 
       update: function (note) {
-        return $http.put(apiUrl + '/notes/' + note.id, note);
+        return $http.put(apiUrl + '/notes/' + note._id, note);
       },
 
       remove: function (noteId) {
