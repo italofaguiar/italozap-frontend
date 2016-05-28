8c96f082dd043846107bcf8a16e6e863299821a0
italoaguiar <italofaguiar@gmail.com>
2016-05-28 15:52:47 -0300

    setando para ir do ws.tocae e avancando uma versao da app

--- www/js/backendService.js
+++ www/js/backendService.js
@@ -1,8 +1,8 @@
 angular.module('italoZap.backendService', [])
   .factory('backendService', function () {
 
-    //var apiUrl = 'http://ws.tocae.com.br';
-    var apiUrl = 'http://192.168.0.18:8300';
+    var apiUrl = 'http://ws.tocae.com.br';
+    // var apiUrl = 'http://192.168.0.18:8300';
     var socketIo = io(apiUrl);
 
     return {
